bdd0edd48100a0e7896a96b0629c0c1f17d1fd33
Joel Low <joel@joelsplace.sg>
2016-01-23 16:16:07 +0800

    Implement process CPU and memory limits.
    These can be specified on the command line as arguments -c and -m respectively.

--- base/usr/local/bin/coursemology-evaluate.sh
+++ base/usr/local/bin/coursemology-evaluate.sh
@@ -5,6 +5,37 @@ sigterm_handler() {
 
 trap sigterm_handler SIGTERM
 
+show_help() {
+  cat << EOF
+Usage: ${0##*/} [-h?] [-mMEMORY_LIMIT] [-cCPU_TIME_LIMIT]
+Prepares, compiles, and executes the submission in the /home/coursemology/package directory.
+
+     -h?         Display this help message and exit
+     -m          Memory limit, in kilobytes, or unlimited for no limit.
+     -c          CPU time limit, in seconds, or unlimited for no limit.
+EOF
+}
+
+# Parse command line arguments
+OPTIND=1
+memory_limit='unlimited'
+cpu_time_limit='unlimited'
+
+while getopts "h?m:c:" opt; do
+  case "$opt" in
+  h|\?)
+    show_help
+    exit 0
+    ;;
+  m)
+    memory_limit=$OPTARG
+    ;;
+  c)
+    cpu_time_limit=$OPTARG
+    ;;
+  esac
+done
+
 mkdir /home/coursemology/package
 cd /home/coursemology/package
 chown -R coursemology:coursemology .
@@ -19,7 +50,9 @@ child=$!
 wait "$child"
 return_code=$?; if [[ $return_code != 0 ]]; then exit $return_code; fi
 
-# TODO: Process constraints
+# Apply process memory and CPU time limits
+ulimit -v $memory_limit -t $cpu_time_limit
+ulimit -a
 gosu coursemology make test &
 child=$! 
 wait "$child"
